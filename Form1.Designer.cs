750294fca53bf313c3457853be44383cbd2e1365
Juan-XC7 <113059155+Juan-XC7@users.noreply.github.com>
2024-03-12 23:25:37 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace tic_tac_toe_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.coordinate1_1 = new System.Windows.Forms.PictureBox();
+            this.coordinate1_3 = new System.Windows.Forms.PictureBox();
+            this.coordinate1_2 = new System.Windows.Forms.PictureBox();
+            this.coordinate2_1 = new System.Windows.Forms.PictureBox();
+            this.coordinate2_3 = new System.Windows.Forms.PictureBox();
+            this.coordinate2_2 = new System.Windows.Forms.PictureBox();
+            this.coordinate3_1 = new System.Windows.Forms.PictureBox();
+            this.coordinate3_2 = new System.Windows.Forms.PictureBox();
+            this.coordinate3_3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate1_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate1_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate1_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate2_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate2_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate2_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate3_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate3_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate3_3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // coordinate1_1
+            // 
+            this.coordinate1_1.BackColor = System.Drawing.Color.White;
+            this.coordinate1_1.Location = new System.Drawing.Point(64, 70);
+            this.coordinate1_1.Name = "coordinate1_1";
+            this.coordinate1_1.Size = new System.Drawing.Size(100, 100);
+            this.coordinate1_1.TabIndex = 0;
+            this.coordinate1_1.TabStop = false;
+            this.coordinate1_1.Click += new System.EventHandler(this.coordinate1_1_Click);
+            // 
+            // coordinate1_3
+            // 
+            this.coordinate1_3.BackColor = System.Drawing.Color.White;
+            this.coordinate1_3.Location = new System.Drawing.Point(276, 70);
+            this.coordinate1_3.Name = "coordinate1_3";
+            this.coordinate1_3.Size = new System.Drawing.Size(100, 100);
+            this.coordinate1_3.TabIndex = 1;
+            this.coordinate1_3.TabStop = false;
+            this.coordinate1_3.Click += new System.EventHandler(this.coordinate1_3_Click);
+            // 
+            // coordinate1_2
+            // 
+            this.coordinate1_2.BackColor = System.Drawing.Color.White;
+            this.coordinate1_2.Location = new System.Drawing.Point(170, 70);
+            this.coordinate1_2.Name = "coordinate1_2";
+            this.coordinate1_2.Size = new System.Drawing.Size(100, 100);
+            this.coordinate1_2.TabIndex = 2;
+            this.coordinate1_2.TabStop = false;
+            this.coordinate1_2.Click += new System.EventHandler(this.coordinate1_2_Click);
+            // 
+            // coordinate2_1
+            // 
+            this.coordinate2_1.BackColor = System.Drawing.Color.White;
+            this.coordinate2_1.Location = new System.Drawing.Point(64, 176);
+            this.coordinate2_1.Name = "coordinate2_1";
+            this.coordinate2_1.Size = new System.Drawing.Size(100, 100);
+            this.coordinate2_1.TabIndex = 3;
+            this.coordinate2_1.TabStop = false;
+            this.coordinate2_1.Click += new System.EventHandler(this.coordinate2_1_Click);
+            // 
+            // coordinate2_3
+            // 
+            this.coordinate2_3.BackColor = System.Drawing.Color.White;
+            this.coordinate2_3.Location = new System.Drawing.Point(276, 176);
+            this.coordinate2_3.Name = "coordinate2_3";
+            this.coordinate2_3.Size = new System.Drawing.Size(100, 100);
+            this.coordinate2_3.TabIndex = 4;
+            this.coordinate2_3.TabStop = false;
+            this.coordinate2_3.Click += new System.EventHandler(this.coordinate2_3_Click);
+            // 
+            // coordinate2_2
+            // 
+            this.coordinate2_2.BackColor = System.Drawing.Color.White;
+            this.coordinate2_2.Location = new System.Drawing.Point(170, 176);
+            this.coordinate2_2.Name = "coordinate2_2";
+            this.coordinate2_2.Size = new System.Drawing.Size(100, 100);
+            this.coordinate2_2.TabIndex = 5;
+            this.coordinate2_2.TabStop = false;
+            this.coordinate2_2.Click += new System.EventHandler(this.coordinate2_2_Click);
+            // 
+            // coordinate3_1
+            // 
+            this.coordinate3_1.BackColor = System.Drawing.Color.White;
+            this.coordinate3_1.Location = new System.Drawing.Point(64, 282);
+            this.coordinate3_1.Name = "coordinate3_1";
+            this.coordinate3_1.Size = new System.Drawing.Size(100, 100);
+            this.coordinate3_1.TabIndex = 6;
+            this.coordinate3_1.TabStop = false;
+            this.coordinate3_1.Click += new System.EventHandler(this.coordinate3_1_Click);
+            // 
+            // coordinate3_2
+            // 
+            this.coordinate3_2.BackColor = System.Drawing.Color.White;
+            this.coordinate3_2.Location = new System.Drawing.Point(170, 282);
+            this.coordinate3_2.Name = "coordinate3_2";
+            this.coordinate3_2.Size = new System.Drawing.Size(100, 100);
+            this.coordinate3_2.TabIndex = 7;
+            this.coordinate3_2.TabStop = false;
+            this.coordinate3_2.Click += new System.EventHandler(this.coordinate3_2_Click);
+            // 
+            // coordinate3_3
+            // 
+            this.coordinate3_3.BackColor = System.Drawing.Color.White;
+            this.coordinate3_3.Location = new System.Drawing.Point(276, 282);
+            this.coordinate3_3.Name = "coordinate3_3";
+            this.coordinate3_3.Size = new System.Drawing.Size(100, 100);
+            this.coordinate3_3.TabIndex = 8;
+            this.coordinate3_3.TabStop = false;
+            this.coordinate3_3.Click += new System.EventHandler(this.coordinate3_3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(926, 475);
+            this.Controls.Add(this.coordinate3_3);
+            this.Controls.Add(this.coordinate3_2);
+            this.Controls.Add(this.coordinate3_1);
+            this.Controls.Add(this.coordinate2_2);
+            this.Controls.Add(this.coordinate2_3);
+            this.Controls.Add(this.coordinate2_1);
+            this.Controls.Add(this.coordinate1_2);
+            this.Controls.Add(this.coordinate1_3);
+            this.Controls.Add(this.coordinate1_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate1_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate1_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate1_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate2_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate2_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate2_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate3_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate3_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coordinate3_3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox coordinate1_1;
+        private System.Windows.Forms.PictureBox coordinate1_3;
+        private System.Windows.Forms.PictureBox coordinate1_2;
+        private System.Windows.Forms.PictureBox coordinate2_1;
+        private System.Windows.Forms.PictureBox coordinate2_3;
+        private System.Windows.Forms.PictureBox coordinate2_2;
+        private System.Windows.Forms.PictureBox coordinate3_1;
+        private System.Windows.Forms.PictureBox coordinate3_2;
+        private System.Windows.Forms.PictureBox coordinate3_3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace tic_tac_toe_GUI
+{
+    public partial class Form1 : Form
+    {
+        static string turn = "player1";
+        private int[,] board = new int[3, 3]; // 3x3 matrix representing the Tic Tac Toe board
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string SwitchTurn()
+        {
+            turn = turn == "player1" ? "player2" : "player1";
+            return turn;
+        }
+
+        public static string getPlayerTurn()
+        {
+            return turn;
+        }
+
+        private int CheckForWinner()
+        {
+            // Check rows
+            for (int row = 0; row < 3; row++)
+            {
+                if (board[row, 0] != 0 && board[row, 0] == board[row, 1] && board[row, 0] == board[row, 2])
+                {
+                    return board[row, 0];
+                }
+            }
+
+            // Check columns
+            for (int col = 0; col < 3; col++)
+            {
+                if (board[0, col] != 0 && board[0, col] == board[1, col] && board[0, col] == board[2, col])
+                {
+                    return board[0, col];
+                }
+            }
+
+            // Check diagonals
+            if (board[0, 0] != 0 && board[0, 0] == board[1, 1] && board[0, 0] == board[2, 2])
+            {
+                return board[0, 0];
+            }
+            if (board[0, 2] != 0 && board[0, 2] == board[1, 1] && board[0, 2] == board[2, 0])
+            {
+                return board[0, 2];
+            }
+
+            // No winner found
+            return 0;   
+        }
+
+        private string GetWinner()
+        {
+            int winnerSymbol = CheckForWinner();
+
+            if (winnerSymbol == 1)
+            {
+                return "Player 1";
+            }
+            else if (winnerSymbol == 2)
+            {
+                return "Player 2";
+            }
+            else
+            {
+                return "No winner"; // No winner found
+            }
+        }
+
+
+        private void StorePlay(PictureBox pictureBox, int coordinate1, int coordinate2)
+        {
+            if (board[coordinate1 - 1, coordinate2 - 1] != 0)
+            {
+                return;
+            }
+
+            int playerMove = (turn == "player1") ? 1 : 2;
+            board[coordinate1-1, coordinate2-1] = playerMove; // Fill with player number
+
+            // Update the picture
+            pictureBox.Image = (turn == "player1") ? Properties.Resources.plant : Properties.Resources.zombie;
+
+            string winner = GetWinner();
+
+            if (winner != "No winner")
+            {
+                MessageBox.Show("Winner: " + winner);
+                return;
+            }
+
+            SwitchTurn();
+        }
+
+        private void coordinate1_1_Click(object sender, EventArgs e)
+        {
+            StorePlay((PictureBox)sender, 1, 1);
+        }
+
+        private void coordinate1_2_Click(object sender, EventArgs e)
+        {
+            StorePlay((PictureBox)sender, 1, 2);
+        }
+
+        private void coordinate1_3_Click(object sender, EventArgs e)
+        {
+            StorePlay((PictureBox)sender, 1, 3);
+        }
+
+        private void coordinate2_1_Click(object sender, EventArgs e)
+        {
+            StorePlay((PictureBox)sender, 2, 1);
+        }
+
+        private void coordinate2_2_Click(object sender, EventArgs e)
+        {
+            StorePlay((PictureBox)sender, 2, 2);
+        }
+
+        private void coordinate2_3_Click(object sender, EventArgs e)
+        {
+            StorePlay((PictureBox)sender, 2, 3);
+        }
+
+        private void coordinate3_1_Click(object sender, EventArgs e)
+        {
+            StorePlay((PictureBox)sender, 3, 1);
+        }
+
+        private void coordinate3_2_Click(object sender, EventArgs e)
+        {
+            StorePlay((PictureBox)sender, 3, 2);
+        }
+
+        private void coordinate3_3_Click(object sender, EventArgs e)
+        {
+            StorePlay((PictureBox)sender, 3, 3);
+        }
+    }
+}
